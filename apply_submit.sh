8123a82896f4e7e20beba21631f21059aae91799
tsukasakop <tsukasakop@gmail.com>
2024-07-01 13:11:32 +0900

    🧑‍💻 Extract .go files

---
+++ apply_submit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+ORGN_BR=${ORGN_BR:-utils}
+SBMT_BR=${SBMT_BR:-submit}
+
+git switch $ORGN_BR &> /dev/null
+hashes=`git log --pretty=format:"%h" --reverse -- **/*.go`
+init_hash=`git rev-list $ORGN_BR | tail -1`
+
+if ! git branch | grep " ${SBMT_BR}$" &> /dev/null
+then
+  git branch submit $init_hash
+fi
+git switch $SBMT_BR &> /dev/null
+
+LN=`git rev-list $SBMT_BR | wc -l | sed 's/ //g'`
+for h in $hashes
+do
+  LN=$(($LN - 1))
+  if [ $LN -lt 1 ]
+  then
+    git cherry-pick $h
+  fi
+done
