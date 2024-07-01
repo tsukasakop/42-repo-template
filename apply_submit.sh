34975d880bea0d5babb1e63d2138132de35a433a
tsukasakop <tsukasakop@gmail.com>
2024-07-01 13:59:32 +0900

    🐛 Enable to change submit branch name

--- apply_submit.sh
+++ apply_submit.sh
@@ -3,15 +3,15 @@
 ORGN_BR=${ORGN_BR:-utils}
 SBMT_BR=${SBMT_BR:-submit}
 
-git switch $ORGN_BR &> /dev/null
+git switch $ORGN_BR 1> /dev/null
 hashes=`git log --pretty=format:"%h" --reverse -- **/*.go`
 init_hash=`git rev-list $ORGN_BR | tail -1`
 
 if ! git branch | grep " ${SBMT_BR}$" &> /dev/null
 then
-  git branch submit $init_hash
+  git branch $SBMT_BR $init_hash
 fi
-git switch $SBMT_BR &> /dev/null
+git switch $SBMT_BR 1> /dev/null
 
 LN=`git rev-list $SBMT_BR | wc -l | sed 's/ //g'`
 for h in $hashes
